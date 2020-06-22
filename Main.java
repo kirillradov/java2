31a66dd28f9b7e3f9fef49a211da26beefeabe9d
kirillradov <46073708+kirillradov@users.noreply.github.com>
2020-06-22 19:48:23 +0300

    master

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ package ru.geekbrains.java2.lesson1;
 
 public class Main {
     public static void main(String[] args) {
+		
+		import ru.geekbrains.java2.lesson1.competitor.*;
+import ru.geekbrains.java2.lesson1.course.Course;
+import ru.geekbrains.java2.lesson1.course.Cross;
+import ru.geekbrains.java2.lesson1.course.Wall;
+import ru.geekbrains.java2.lesson1.course.Water;
 
     }
 }
